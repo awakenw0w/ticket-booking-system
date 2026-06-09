d3c07b13f9c4d1e10b2d24e45a65796d3c0640ca
awakenw0w <185328219+awakenw0w@users.noreply.github.com>
2026-06-09 23:53:09 +0300

    Рабочая версия автозапуска

--- Launch.bat
+++ Launch.bat
@@ -1,19 +1,21 @@
 @echo off
-chcp 65001 > nul
 
-cd /d "%~dp0backend"
+set "PROJECT=%~dp0"
+
+pushd "%PROJECT%backend"
 call composer install
-copy .env.example .env /Y
+copy /Y .env.example .env
 php artisan key:generate
-type nul > ".\database\database.sqlite"
+type nul > "database\database.sqlite"
 php artisan migrate:fresh --seed
+popd
 
-start "Backend" cmd /k "cd /d ""%~dp0backend"" && php artisan serve --host=127.0.0.1 --port=8000"
-start "Frontend" cmd /k "cd /d ""%~dp0frontend"" && python -m http.server 5173 --bind 127.0.0.1"
+start "Backend" /D "%PROJECT%backend" cmd /k php artisan serve --host=127.0.0.1 --port=8000
+start "Frontend" /D "%PROJECT%frontend" cmd /k python -m http.server 5173 --bind 127.0.0.1
 
 echo.
-echo Проект запущен.
-echo Открой в браузере:
+echo PROJECT STARTED
+echo FRONTEND URL:
 echo http://127.0.0.1:5173
 echo.
 pause
